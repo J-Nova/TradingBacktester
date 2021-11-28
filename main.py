3c4387ec38e09e91deb5b476adab49dd93fbc67c
Juup Pikkemaat <Pikkemaatjuup@gmail.com>
2021-11-28 20:59:36 +0100

    makes a backtest object now

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from historical_data import GetHistoricalData
 import configparser
 import pickle
+import os
+from datetime import datetime
 config = configparser.ConfigParser()
 
-SELECTION = ["1", "2", "0"]
 
 class strategy(object):
-    def __init__(self, strategy_name):
+    def __init__(self, strategy_name, stop_loss, take_profit):
         self.name = strategy_name
         self.strats_in_use = []
+        self.stop_loss = stop_loss
+        self.take_profit = take_profit
 
 
     def rsi(self, period, buy, sell):
@@ -16,14 +19,53 @@ class strategy(object):
         self.rsi_buy = buy
         self.rsi_sell = sell
         self.strats_in_use.append("rsi")
-
-
+        
     def __str__(self):
         return(f"Strategy name: {self.name}, indicators in use: {self.strats_in_use}")
 
+class backtest(object):
+    def __init__(self, symbol, interval, year, month, strategy_object):
+        self.__symbol = symbol
+        self.__interval = interval
+        self.__year = year
+        self.__month = month
+        self.__strategy = strategy_object
+        self.buys = 0
+        self.sells = 0
+        self.left_open = 0
+        self.profit = 0
+        self.avg_holding_time = 0
+        self.backtest_date = datetime.now()
+    
+    @property
+    def symbol(self):
+        return self.__symbol
+    @property
+    def interval(self):
+        return self.__interval
+    @property
+    def year(self):
+        return self.__year
+    @property
+    def month(self):
+        return self.__month
+    @property
+    def strategy(self):
+        return self.__strategy
+    
+    def create_simulation(self):
+        pass
+    
+    
+    def __str__(self):
+        return f"Backtest ran on: {self.backtest_date}."
+
 def config_editor(indicators):
     strategy_name = input("What is the name of the strategy: ")
-    strategy_config = strategy(strategy_name)
+    stop_loss = input("Stop loss % (Leave empty if not applicable): ")
+    take_profit = input("Take profit %: ")
+    strategy_config = strategy(strategy_name, stop_loss, take_profit)
+    
     for indicator in indicators:
         if indicator == "rsi":
             # Prints current data for rsi indicator and asks for new settings.
@@ -33,19 +75,31 @@ def config_editor(indicators):
             strategy_config.rsi(rsi_period, rsi_buy, rsi_sell)
     
     # writes the changes to the strategy file.
-    # with open(f"strategies/{strategy_name}.ini", 'wb') as configfile:
-    #     configfile.truncate(0)
-    #     pickle.dump(strategy_config, configfile)
-    #     print("Saved strategy configuration")
+    # strategy_name = strategy_name.replace(" ", "_")
+    with open(f"strategies/{strategy_name}.strategy", 'wb') as configfile:
+        configfile.truncate(0)
+        pickle.dump(strategy_config, configfile)
+    print("Saved strategy configuration\n")
     print(strategy_config)
 
 
+def available_strategies():
+    available_strategies = []
+    directory = "strategies"
+    for filename in os.scandir(directory):
+        if filename.is_file():
+            strategy_name, _ = os.path.splitext(filename)
+            strat_name = strategy_name.split("/")[1]
+            available_strategies.append(strat_name)
+    return available_strategies
+
 def main(config):
+    selection = ["1", "2", "0"]
     while True:
-        print("0: Exit program.\n1: Collect historical data.\n2: Create or edit a strategy.")
+        print("0: Exit program.\n1: Run backtest on historical data.\n2: Create or edit a strategy.")
         while True:
             user_selection = input("What would you like to do: ")
-            if user_selection in SELECTION:
+            if user_selection in selection:
                 break
         
         if user_selection == "1":
@@ -59,11 +113,25 @@ def main(config):
             else:
                 symbol = input("Symbol: Example; BTCUSDT: ")
                 interval = input("Interval: Example; 5m: ")
-                year = input("Year: Example; 2021")
-                month = input("Month: Example; 09")
+                year = input("Year: Example; 2021: ")
+                month = input("Month: Example; 09: ")
             # Downloads historical data based on the setting provided from the binance and stores in a csv file.
             historical_data = GetHistoricalData(symbol, interval, year, month)
-            print(historical_data)
+            strats = available_strategies()
+            for index, strategy in enumerate(strats):
+                print(f"{index} - {strategy}")
+            selected = input("Please select a strategy to use with backtesting: ")
+            while not selected.isdigit() or int(selected) not in range(0, len(strats)):
+                selected = input("Please select a strategy to use with backtesting: ")
+                
+            selected = int(selected)
+            strategy = strats[selected]
+            print(f"Running backtest with {strats[selected]} on {historical_data.split('/')[1]}")
+            with open(f"strategies/{strategy}.strategy",'rb') as file:
+                    strategy_object = pickle.load(file)
+            backtest_object = backtest(symbol, interval, year, month, strategy_object)
+            print(backtest_object)
+
             
         elif user_selection == "2":
             # Get all available indicators and print them.
